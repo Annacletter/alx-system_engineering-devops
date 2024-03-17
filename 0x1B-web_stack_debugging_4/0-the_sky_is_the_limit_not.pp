537c5164f6848ad1b2b3793cda23e40b44f89e1f
root <anna.cletter12@gmail.com>
2024-03-17 07:58:05 -0700

    changes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,13 @@
 # Adjust ULIMIT for Nginx
 exec { 'adjust-nginx-ulimit':
-  command => 'sed -i "s/some_parameter/new_value/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/',
+  command => 'sed -i "s/worker_connections\s*\(.*\);/worker_connections 100;/g" /etc/nginx/nginx.conf',
+  path    => ['/usr/local/bin', '/bin', '/usr/bin'],
+  notify  => Service['nginx'],
 }
 
-# Restart Nginx
+# Restart Nginx when the configuration changes
 service { 'nginx':
   ensure    => 'running',
   enable    => true,
-  subscribe => Exec['adjust-nginx-ulimit'],
+  subscribe => File['/etc/nginx/nginx.conf'],
 }
