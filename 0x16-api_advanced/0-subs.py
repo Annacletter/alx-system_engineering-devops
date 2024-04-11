a16d6bbf90dc6e5d3e99a6fd357701f1c33d382c
root <anna.cletter12@gmail.com>
2024-04-11 13:27:59 -0700

    amend

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-        Queries Reddit API and returns "OK" if the subreddit exists,else "KO".
+        Queries Reddit API and returns "0" if the subreddit exists,else "1".
         @param subreddit: Name of the subreddit
         @return: "OK" if subreddit exists, "KO" otherwise
     """
@@ -17,12 +17,12 @@ def number_of_subscribers(subreddit):
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
-            return "OK"
+            return "0"
         else:
-            return "KO"
+            return "1"
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
-        return "KO"
+        return "1"
 
 
 if __name__ == "__main__":
