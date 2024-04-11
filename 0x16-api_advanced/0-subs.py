ea60371d662a4a806e0933f9ed41af0727825ed9
root <anna.cletter12@gmail.com>
2024-04-11 13:35:12 -0700

    amend

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python3
-"""
-    Queries the Reddit API
-"""
+"""Query Reddit API for number of subscribers on Reddit subreddit."""
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """
-        Queries Reddit API and returns "0" if the subreddit exists,else "1".
-        @param subreddit: Name of the subreddit
-        @return: "OK" if subreddit exists, "KO" otherwise
-    """
-    url = f"https://api.reddit.com/r/{subreddit}/about"
-    headers = {'User-Agent': 'CustomClient/1.0'}
-
+    '''
+        returns the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'customer_client'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            return "0"
-        else:
-            return "1"
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return "1"
+        return url.get('data').get('subscribers')
+    except Exception:
+        return 0
 
 
 if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print(number_of_subscribers(sys.argv[1]))
+    number_of_subscribers(argv[1])
