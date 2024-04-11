cf13eefcc1303e9941e02ca88e05450997af3cf4
root <anna.cletter12@gmail.com>
2024-04-11 13:47:07 -0700

    amend

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 """
-    quering the Reddit API
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
 """
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """
-        Function that queries the Reddit API and returns the number
-        of subscribers.
-        @subreddit: suscriptors
-    """
-    url = "https://api.reddit.com/r/{}/about".format(subreddit)
-    header = {'User-Agent': 'CustomClient/1.0'}
-    request = requests.get(url, headers=header, allow_redirects=False)
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-    if request.status_code != 200:
-        return 0
-    jreq = request.json()
+    headers = {
+        'User-Agent': u_agent
+    }
 
-    if 'data' in jreq:
-        return jreq.get("data").get("subscribers")
-    else:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
