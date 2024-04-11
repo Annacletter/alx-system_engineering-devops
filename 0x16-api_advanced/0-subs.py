ad8926070581dd7a60774bfa28d608d61ddf1393
root <anna.cletter12@gmail.com>
2024-04-11 13:39:09 -0700

    amend

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Query Reddit API for number of subscribers on Reddit subreddit."""
+"""
+    quering the Reddit API
+"""
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    '''
-        returns the number of subscribers for a given subreddit
-    '''
-    user = {'User-Agent': 'customer_client'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
+    """
+        Function that queries the Reddit API and returns the number
+        of subscribers.
+        @subreddit: suscriptors
+    """
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    header = {'User-Agent': 'CustomClient/1.0'}
+    request = requests.get(url, headers=header, allow_redirects=False)
+
+    if request.status_code != 200:
         return 0
+    jreq = request.json()
 
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+    if 'data' in jreq:
+        return jreq.get("data").get("subscribers")
+    else:
+        return 0
