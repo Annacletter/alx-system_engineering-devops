605181c6d2ad2b5eb27fc371625ce3dc7f112667
root <anna.cletter12@gmail.com>
2024-04-11 13:02:46 -0700

    amend

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Querying the Reddit API
+    quering the Reddit API
 """
 import requests
 
@@ -9,18 +9,17 @@ def number_of_subscribers(subreddit):
     """
         Queries the Reddit API and returns the number
         of subscribers.
-        @param subreddit: Name of the subreddit
-        @return: Number of subscribers, or 0 if subreddit not found or error
+        @subreddit: suscriptors
     """
-    url = f"https://api.reddit.com/r/{subreddit}/about"
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise error for bad status codes
-        data = response.json()
-        return data.get("data", {}).get("subscribers", 0)
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return 0
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    header = {'User-Agent': 'CustomClient/1.0'}
+    request = requests.get(url, headers=header, allow_redirects=False)
 
+    if request.status_code != 200:
+        return 0
+    jreq = request.json()
+
+    if 'data' in jreq:
+        return jreq.get("data").get("subscribers")
+    else:
+        return 0
